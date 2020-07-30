0346e1a068b6b142379fa86ea9228df5fe67fcb5
AlyonaKlekovkina <50324020+AlyonaKlekovkina@users.noreply.github.com>
2020-07-30 16:38:37 +0300

    Add files via upload
    Added functions and brushed up the code

--- todolist.py
+++ todolist.py
@@ -9,6 +9,76 @@ engine = create_engine('sqlite:///todo.db?check_same_thread=False')
 Base = declarative_base()
 
 
+def show_today_tasks():
+    day = datetime.today().date()
+    today_tasks = session.query(Task).filter(Task.deadline == day).all()
+    if not today_tasks:
+        print("\nToday ", day.strftime("%-d %b"), ":\nNothing to do!\n", sep="")
+    else:
+        for today_task in today_tasks:
+            print(today_task.id, ". ", today_task.task, ". ", today_task.deadline.strftime("%-d %b"), sep="")
+
+
+def show_weeks_tasks():
+    the_day = datetime.today().date()
+    for day in range(7):
+        needed_day = the_day + timedelta(days=day)
+        needed_day_task = session.query(Task).filter(Task.deadline == needed_day).all()
+        if not needed_day_task:
+            print("\n", needed_day.strftime("%A %d %b"), ":\nNothing to do!", sep="")
+        else:
+            print("\n", needed_day.strftime("%A %d %b"), sep="")
+            i = 1
+            for task in needed_day_task:
+                print(i, ". ", task.task, sep="")
+                i += 1
+
+
+def show_all_tasks():
+    print("All tasks:")
+    all_tasks = session.query(Task).order_by(Task.deadline).all()
+    for task in all_tasks:
+        print(task.id, ". ", task.task, ". ", task.deadline.strftime("%-d %b"), sep="")
+
+
+def show_missed_tasks():
+    print("Missed tasks:")
+    day = datetime.today().date()
+    missed_tasks = session.query(Task).filter(Task.deadline < day).all()
+    if not missed_tasks:
+        print("Nothing is missed!")
+    else:
+        for missed in missed_tasks:
+            print(missed.id, ". ", missed.task, ". ", missed.deadline.strftime("%-d %b"), sep="")
+
+
+def add_task():
+    task_entry = input("Enter task\n")
+    date_entry = input('Enter deadline\n')
+    deadline_year, current_month, current_day = map(int, date_entry.split('-'))
+    date1 = date(deadline_year, current_month, current_day)
+    new_task = Task(task=task_entry, deadline=date1)
+    session.add(new_task)
+    session.commit()
+    print("The task has been added!")
+
+
+def delete_task():
+    all_tasks = session.query(Task).order_by(Task.deadline).all()
+    if not all_tasks:
+        print("Nothing to delete")
+    else:
+        print("Chose the number of the task you want to delete:")
+        show_all_tasks()
+
+    to_delete = int(input())
+    for to_be_deleted in all_tasks:
+        if to_delete == to_be_deleted.id:
+            session.query(Task).filter(Task.id == to_be_deleted.id).delete()
+            session.commit()
+            print("The task has been deleted!")
+
+
 class Task(Base):
     __tablename__ = 'task'
     id = Column(Integer, primary_key=True)
@@ -24,103 +94,24 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 while True:
-    users_input = input("\n1) Today's tasks\n2) Week's tasks\n3) All tasks\n4) Add task\n0) Exit\n")
-    all_tasks = session.query(Task).order_by(Task.deadline).all()
-    the_day = datetime.today().date()
-    today = datetime.today()
-    current_day = today.day
-    current_month = today.strftime('%b')
-    today_tasks = session.query(Task).filter(Task.deadline == the_day).all()
-    last_day = the_day + timedelta(days=6)
-    week_tasks = session.query(Task).filter(Task.deadline.between(the_day, last_day)).order_by(Task.deadline).all()
-
+    users_input = input("\n1) Today's tasks\n2) Week's tasks\n3) All tasks\n4) Missed tasks\n5) Add task\n6) Delete task\n0) Exit\n")
     if users_input == '1':
-        if not today_tasks:
-            print("\nToday ", current_day, " ", current_month, ":\nNothing to do!\n", sep="")
-        else:
-            for today_task in today_tasks:
-                print(today_task.id, ". ", today_task.task, ". ", today_task.deadline.strftime("%-d %b"), sep="")
+        show_today_tasks()
         continue
     elif users_input == '2':
-        first_day = datetime.today().date()
-        first_day_task = session.query(Task).filter(Task.deadline == first_day).all()
-        if not first_day_task:
-            print("\n", first_day.strftime("%A %-d %b"), ":\nNothing to do!", sep="")
-        else:
-            print("\n", first_day.strftime("%A %-d %b"), ":", sep="")
-            i = 1
-            for task in first_day_task:
-                print(i, ". ", task.task, sep="")
-        second_day = the_day + timedelta(days=1)
-        second_day_task = session.query(Task).filter(Task.deadline == second_day).all()
-        if not second_day_task:
-            print("\n", second_day.strftime("%A %-d %b"), ":\nNothing to do!", sep="")
-        else:
-            print("\n", second_day.strftime("%A %-d %b"), ":", sep="")
-            i = 1
-            for task in second_day_task:
-                print(i, ". ", task.task, sep="")
-        third_day = the_day + timedelta(days=2)
-        third_day_task = session.query(Task).filter(Task.deadline == third_day).all()
-        if not third_day_task:
-            print("\n", third_day.strftime("%A %-d %b"), ":\nNothing to do!", sep="")
-        else:
-            print("\n", third_day.strftime("%A %-d %b"), ":", sep="")
-            i = 1
-            for task in third_day_task:
-                print(i, ". ", task.task, sep="")
-        forth_day = the_day + timedelta(days=3)
-        forth_day_task = session.query(Task).filter(Task.deadline == forth_day).all()
-        if not forth_day_task:
-            print("\n", forth_day.strftime("%A %-d %b"), ":\nNothing to do!", sep="")
-        else:
-            print("\n", forth_day.strftime("%A %-d %b"), ":", sep="")
-            i = 1
-            for task in forth_day_task:
-                print(i, ". ", task.task, sep="")
-        fifth_day = the_day + timedelta(days=4)
-        fifth_day_task = session.query(Task).filter(Task.deadline == fifth_day).all()
-        if not fifth_day_task:
-            print("\n", fifth_day.strftime("%A %-d %b"), ":\nNothing to do!", sep="")
-        else:
-            print("\n", fifth_day.strftime("%A %-d %b"), ":", sep="")
-            i = 1
-            for task in fifth_day_task:
-                print(i, ". ", task.task, sep="")
-        six_day = the_day + timedelta(days=5)
-        six_day_task = session.query(Task).filter(Task.deadline == six_day).all()
-        if not six_day_task:
-            print("\n", six_day.strftime("%A %-d %b"), ":\nNothing to do!", sep="")
-        else:
-            print("\n", six_day.strftime("%A %-d %b"), ":", sep="")
-            i = 1
-            for task in six_day_task:
-                print(i, ". ", task.task, sep="")
-        seventh_day = the_day + timedelta(days=6)
-        seventh_day_task = session.query(Task).filter(Task.deadline == seventh_day).all()
-        if not seventh_day_task:
-            print("\n", seventh_day.strftime("%A %-d %b"), ":\nNothing to do!", sep="")
-        else:
-            print("\n", seventh_day.strftime("%A %-d %b"), ":", sep="")
-            i = 1
-            for task in seventh_day_task:
-                print(i, ". ", task.task, sep="")
-                i += 1
+        show_weeks_tasks()
         continue
     elif users_input == '3':
-        print("All tasks:")
-        for all_task in all_tasks:
-            print(all_task.id, ". ", all_task.task, ". ", all_task.deadline.strftime("%-d %b"), sep="")
+        show_all_tasks()
         continue
     elif users_input == '4':
-        task_entry = input("Enter task\n")
-        date_entry = input('Enter deadline\n')
-        deadline_year, current_month, current_day = map(int, date_entry.split('-'))
-        date1 = date(deadline_year, current_month, current_day)
-        new_task = Task(task=task_entry, deadline=date1)
-        session.add(new_task)
-        session.commit()
-        print("The task has been added!")
+        show_missed_tasks()
+        continue
+    elif users_input == '5':
+        add_task()
+        continue
+    elif users_input == '6':
+        delete_task()
         continue
     elif users_input == '0':
         print("Bye!")
